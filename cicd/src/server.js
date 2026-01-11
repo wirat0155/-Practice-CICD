81a170b732a67bed031c973527ee13100604dd94
CICD Student <student@cicd.com>
2026-01-11 09:14:06 +0700

    Add proper linting and artifact build workflow

--- cicd/src/server.js
+++ cicd/src/server.js
@@ -3,5 +3,6 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
     console.log(`Server is running on port ${PORT}`);
 });
